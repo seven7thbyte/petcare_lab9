3fe33084588c395db42a600d8649a933c40aadd2
Dazai-arch <alok.tayal@mca.christuniversity.in>
2025-08-23 00:11:35 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: true
